975728dfe96aeb79146ba802a37c2d52d1951b1f
[Hamid] <[heidarzadeh.hamidreza@gmail.com]>
2021-06-16 09:53:23 -0700

    added all the files

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.post("/api/registerUser", function(req,res){
     res.status(200).send({"message":"You are in /api/getUsers => Method: POST"})
 })
 
+//FrontEnd
+var path = require('path');
+app.use('/',express.static(path.join(__dirname,'src')));
+
+//404
 app.use('*', function(req,res){
     res.status(404).send({"message": "the route does not exist my friend!"})
 })
